a81c2e88dde5bfc6efac13960e1d688c65bb3689
josefabiodev <josefabiodev@gmail.com>
2023-10-24 09:52:05 -0300

    Erros

---
+++ erros/erros.js
@@ -0,0 +1,9 @@
+// SyntaxError
+
+//const numero; -> const não inicializada
+
+//console.log("numero" -> falta fechar parênteses
+
+
+/* ReferenceError */
+console.log(minhaVar); // variável usada, mas não declarada
