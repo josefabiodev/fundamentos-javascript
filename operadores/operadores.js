66592b856fb04894a45c96964068078b99830a91
josefabiodev <josefabiodev@gmail.com>
2023-10-24 10:24:49 -0300

    operador ternário, template string

--- operadores/operadores.js
+++ operadores/operadores.js
@@ -15,3 +15,48 @@ console.log(typeof text);
 // Conversão explícita
 Number();
 String();
+
+/*
+    adição -> +
+    subtração -> -
+    multiplicação -> *
+    divisão -> /
+
+    de atribuição -> =
+    de comparação -> == e ===
+
+    Operadores lógicos
+
+    ou -> ||
+    e -> &&
+
+    Operadores comparação
+    igual -> == ou ===
+    maior -> >
+    menor -> <
+    maior igual -> >=
+    menor igual -> <=
+    não igual -> !=
+    estritamente não migual -> !==
+*/
+
+// Operador ternário
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= idadeMinima) {
+    console.log("Pode beber.")
+} else {
+    console.log("Não pode beber.")
+}
+
+// com operador ternario
+console.log(idadeCliente >= idadeMinima ? "Pode beber" : "Não pode beber");
+
+// Template literal - template string
+const nome = "Fábio";
+const apresentacao = `Meu nome é ${nome}`;
+
+console.log(apresentacao);
+
